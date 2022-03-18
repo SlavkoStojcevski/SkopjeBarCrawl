0b799ce9ce7c6a128dd8bafe6e753465487ad6dc
SlavkoStojcevski <slavko.stojcevski1990@gmail.com>
2022-03-18 12:06:49 +0100

    navbar

--- index.php
+++ index.php
@@ -7,12 +7,19 @@
 
     <!-- Bootstrap CSS -->
     <link href="views/bootstrap.css" rel="stylesheet">
+    <link href="views/styles.css" rel="stylesheet">
 
+    <link rel="shortcut icon" href="views/logo.jpg" type="image/x-icon">
     <title>Skopje Bar Crawl</title>
   </head>
   <body>
+    <audio id="myAudio"><source src="views/beer.mp3"></audio>
     <?php include("views/navbar.php");?>
+    <?php include("views/About.php");?>
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script>
+      function play(){document.getElementById("myAudio").play();}
+    </script>
   </body>
 </html>
--- views/navbar.php
+++ views/navbar.php
@@ -1,1 +1,7 @@
-<nav class="navbar navbar-default navbar">Test</nav>
+<nav>
+    <div class="titles"><h1><a href="#About">About the tours</a></h1></div>
+    <div class="titles"><h1><a href="#Team">Our Team</a></h1></div>
+    <div onclick ="play()" id="logo"></div>
+    <div class="titles"><h1><a href="#Testimonials">Testimony</a></h1></div>
+    <div class="titles"><h1><a href="#Contact">Contact us</a></h1></div>
+</nav>
